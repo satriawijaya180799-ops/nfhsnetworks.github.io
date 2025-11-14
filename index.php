3bb9176cd5b3dfe5521cf14ae9755e078ed55bdd
satriawijaya180799-ops <satriawijaya.180799@gmail.com>
2025-11-14 06:47:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,281 @@
+<?php require_once('pengaturan.php'); ?>
+<html class="no-js en">
+<meta charset="utf-8">
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1,user-scalable=1">
+<meta name="robots" content="NOINDEX, NOFOLLOW, NOARCHIVE, NOODP, NOIMAGEINDEX, NOSNIPPET">
+<title> STREAMING LIVE SPORTS LIVE </title>
+<link rel="stylesheet" href="/assets/css/style.css">
+<script src="http://go.donnaplay.com/js/signup/movies/gy/modernizr.js"></script>
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="https://i.imgur.com/UtE9eZe.png">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+<style>
+    body {
+        background: linear-gradient(rgba(0, 0, 0, .6), rgba(0, 0, 0, .6)), url(https://library.sportingnews.com/styles/crop_style_16_9_desktop/s3/2025-02/NFHS%20logo%203.png?itok=Ixaw1tbU) no-repeat center center fixed;
+        background-size: cover;
+    }
+
+    header {
+        background: url(/images/head2.jpg);
+    }
+
+    .popup-time {
+        background: url(https://library.sportingnews.com/styles/crop_style_16_9_desktop_webp/s3/2025-02/NFHS%20logo%202.png.webp?itok=u5ZYZDtG) no-repeat center center;
+        background-size: cover;
+    }
+
+    .popup-container {
+        background: linear-gradient(rgba(0, 0, 0, .6), rgba(0, 0, 0, .6)), url(https://library.sportingnews.com/styles/crop_style_16_9_desktop_webp/s3/2025-02/NFHS%20logo%202.png.webp?itok=u5ZYZDtG) no-repeat center center;
+        background-size: cover;
+    }
+</style>
+
+</head>
+
+<body>
+    <!-- Content -->
+    <div class="body-div">
+        <header id="header" class="color-span">
+            <div class="row">
+                <div class="large-12 columns">
+                    <div class="row">
+                        <div class="logo logo-triangle">
+                            <img src="https://nfhs.network-sports.com/images/Capture.PNG" alt="Sport Live">
+                            <p><span class="logo-text"></span></p>
+                        </div>
+                        <a href="/register.php" class="right triangle-right"><i class="fa fa-user"></i>Login</a>
+                    </div>
+                </div>
+            </div>
+            <div class="color-span-wrap color-span-wrap--bottom">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </header>
+        <div class="container">
+            <div class="video row">
+                <center>
+                    <a href="/register.php">
+                        <div class="header-wrapper" style="margin-top:10px;">
+                            <img src="" alt="">
+                            <center>
+
+                                    <?php
+                                    // if isset $_GET['live'] then show the title else show "Lve Stream"
+                                    if (isset($_GET['live'])) {
+                                        echo $_GET['live'];
+                                    } else {
+
+                                    }
+                                    ?>
+                                </h1>
+                            </center>
+                        </div>
+                        <h1 class="text-center media-heading" style="color:white;"></h1>
+                        <center><?php include 'ads.php'; ?></center>
+                    </a>
+                    <div class="video-screen">
+                        <a href="/register.php">
+                            <div class="overlay-video"></div>
+                            <div class="percent-container">
+                                <p><img class="loader" src="/images/video-throbber.gif" alt=""></p>
+                                <p class="percent"></p>
+                            </div>
+                            <div class="popup-time">
+                                <p>Starts in <span id="countdown">00:08</span></p>
+                            </div>
+                        </a>
+                        <div class="popup-container">
+                            <a href="/register.php">
+                                <div class="popup-text">Sport High School Sports Live Is Only Available For Registered Users. Create a FREE account to get access to your HD stream</div>
+                            </a>
+                            <a href="/register.php" target="_blank" class="x-domain">
+                                <div class="popup-button">CREATE A FREE ACCOUNT</div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="controls">
+                        <div class="small-1 medium-1 large-1 no-col-hd-pad columns pause-play"><i class="icon-play"></i><a href="#" target="_blank" class="x-domain"><i class="icon-pause"></i></a></div>
+                        <div class="small-8 medium-7 large-6 columns">
+                            <div class="time">
+                                <div class="time-full buffer-play"></div>
+                                <div class="time-buffer buffer"></div>
+                            </div>
+                        </div>
+                        <div class="small-1 medium-1 columns hide-for-large-up volume-icon"><i class="icon-vol"></i></div>
+                        <div class="small-0 medium-0 large-3 columns no-col-left-pad show-for-large-up vol-wrapper">
+                            <div class="small-0 medium-0 large-1 columns"><i class="icon-vol"></i></div>
+                            <div class="small-0 medium-0 medium-9 columns"><input type="text" data-slider="true" value="0.4" data-slider-highlight="true" style="display:none;"></div>
+                        </div>
+                        <a href="#" target="_blank" class="x-domain fullscreen"><i class="icon-fullscreen"></i></a>
+                        <div class="small-1 medium-1 large-1 no-col-hd-pad columns live-msg">
+                            <img src="/images/red-dot.png" class="red-dot"> LIVE
+
+            </div>
+            <p class="copyrights">
+        </div>
+        </div>
+			<div class="text-center">
+				<center><p><img src="https://ohsaa.stream-sports.site/images/available-formats-img.png" class="img-responsive"></p>
+				
+        </div>
+			
+							<div class="modal-footer">
+					<div class="text-center">
+						<button class="btn btn-warning btn-lg btn-responsive" onclick="window.location.href='https://api.powerafftrky.com/t/clk?id=2dt2mnUznwUggBsX&s1=subaffiliate&s2=clickid'">
+							
+								<span class="icon icon-arrow-right-circle"></span>
+							</span> Become a subscriber to watch your favorite high school games from across the country Live and On Demand on any device
+						</button>
+                    </p>
+            </a>
+            <center>
+<center><?php require_once('iklan.html');?></center>
+                </a>
+                <left>
+        </div>
+        <div class="row" style="max-width: 74em;margin-bottom: 20px;background: #00000080;padding: 15px 5px 5px 5px;">
+            <div id="google_translate_element" style="float:left; margin-right:5px;"></div>
+            <!-- AddThis Button BEGIN -->
+            <div class="addthis_toolbox addthis_default_style" style="float:left">
+                <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+                <a class="addthis_button_tweet"></a>
+                <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
+                <a class="addthis_counter addthis_pill_style"></a>
+            </div>
+            <script type="text/javascript">
+                var addthis_config = {
+                    "data_track_addressbar": true
+                };
+            </script>
+            <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5156a09e76c1568f"></script>
+            <!-- AddThis Button END -->
+        </div>
+        </p>
+        <p>
+            </footer>
+            <center>
+                </a>
+                <left>
+                    <div id="google_translate_element"></div>
+                    <script type="text/javascript">
+                        function googleTranslateElementInit() {
+                            new google.translate.TranslateElement({
+                                pageLanguage: 'en',
+                                layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                                multilanguagePage: true,
+                                gaTrack: true,
+                                gaId: 'UA-240827849-1'
+                            }, 'google_translate_element');
+                        }
+                    </script>
+                    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                    <center>
+                        </a>
+                        <left>
+                            </script>
+                            <footer class="col-md-12" style="background:transparent;padding: 20px;color: whaite;font-weight: bolder;text-shadow: 0px  0px 1px rgb(0, 0, 0);">
+                                | <a style="color:#fbfcfa;" href="/contact.php">Contact Us</a> | <a style="color:#fbfcfa;" href="/dmca.php">DMCA</a> | <a style="color:#fbfcfa;" href="/privacy.php">Privacy</a> | <a style="color:#fbfcfa;" href="<?php echo $url_web; ?>">Copyright NETWORK SPORTS 2025©</a></r>
+    </div>
+    <!-- Histats.com  START  (aync)-->
+    <script type="text/javascript">
+        var _Hasync = _Hasync || [];
+        _Hasync.push(['Histats.start', '1,<?php echo $id_histats; ?>,4,0,0,0,00010000']);
+        _Hasync.push(['Histats.fasi', '1']);
+        _Hasync.push(['Histats.track_hits', '']);
+        (function() {
+            var hs = document.createElement('script');
+            hs.type = 'text/javascript';
+            hs.async = true;
+            hs.src = ('//s10.histats.com/js15_as.js');
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+        })();
+    </script>
+    <noscript><a href="/" target="_blank"><img src="//sstatic1.histats.com/0.gif?<?php echo $id_histats; ?>&101" alt="counter create hit" border="0"></a></noscript>
+    <!-- Histats.com  END  --> </footer>
+    </div>
+    </div>
+    <style>
+        .online {
+            color: #fff;
+            font-size: 12px;
+            position: fixed;
+            bottom: 0;
+            right: 0;
+            background: rgba(0, 0, 0, 0.5);
+            width: 190px;
+            padding: 10px;
+            border-radius: 5px 0 0 0;
+        }
+
+        .online-icon {
+            display: inline-block;
+            background: url('/circle_green.png') no-repeat;
+            width: 14px;
+            height: 20px;
+        }
+
+        .online-text {
+            width: 145px;
+            float: right;
+            display: inline-block;
+        }
+
+        .online-text p {
+            margin: 0;
+        }
+    </style>
+
+    <div class="online">
+        <div class="online-icon"></div>
+        <div class="online-text">
+            <p><span id="online">7411</span> Users Online Now</p>
+            <script type="text/javascript">
+                setInterval(function() {
+                            random = (Math.floor((Math.random() * 15) + 1));
+                            var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+                            random = random * plusOrMinus;
+                            currentnumber = document.getElementById('online');
+
+                            document.getElementById('online').innerHTML = parseInt(currentnumber.innerHTML) + random;
+
+
+                            <
+                            /body> < /
+                            html > < /footer > < /
+                            div > < /
+                            style >
+
+                                <
+                                div class = "online" > < div class = "online-icon" > < /div><div class="online-text"><p><span id="online">7411</span > Users Online Now < /p> <
+                            script type = "text/javascript" >
+                                setInterval(function() {
+                                    random = (Math.floor((Math.random() * 15) + 1));
+                                    var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+                                    random = random * plusOrMinus;
+                                    currentnumber = document.getElementById('online');
+
+                                    document.getElementById('online').innerHTML = parseInt(currentnumber.innerHTML) + random;
+
+                                }, 2500);
+            </script>
+        </div>
+    </div>
+    <script src="/js/js.js"></script>
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+            }, 'google_translate_element');
+        }
+    </script>
+    <script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+</body>
+
+
+</html>
